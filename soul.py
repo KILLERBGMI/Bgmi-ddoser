7c38b2b9ed3d304e8971b31c7d435b8d8ac23ea3
KILLERBGMI <sexop101@outlook.com>
2025-01-01 07:40:20 +0530

    Update soul.py

--- soul.py
+++ soul.py
@@ -16,11 +16,11 @@ from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
 loop = asyncio.get_event_loop()
 
-TOKEN = ''
+TOKEN = '7151529676:AAHjg4UKekjZqLdXsveFIfkOHU4OUSV0iVU'
 MONGO_URI = 'mongodb+srv://Bishal:Bishal@bishal.dffybpx.mongodb.net/?retryWrites=true&w=majority&appName=Bishal'
-FORWARD_CHANNEL_ID = -1002156421934
-CHANNEL_ID = -1002156421934
-error_channel_id = -1002156421934
+FORWARD_CHANNEL_ID = -1001854296925
+CHANNEL_ID = -1001854296925
+error_channel_id = -1001854296925
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
